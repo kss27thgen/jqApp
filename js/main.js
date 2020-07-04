e7c8f50e53d04258414dbd02689a944c7390a4d7
Oishi Yuikyo <mahayana3pm@gmail.com>
2020-07-04 22:44:41 +0900

    create header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(function () {
-	$(".test").css("color", "blue");
+	$(".header--small").hide();
+
+	$("html").on("click", "#openSearchBox", function () {
+		$(".header--normal").hide();
+		$(".header--small").show();
+	});
+
+	$("html").on("click", "#closeHeaderSmall", function () {
+		$(".header--small").hide();
+		$(".header--normal").show();
+	});
+
+	$(".openSidenav").on("click", function (e) {
+		e.stopPropagation();
+		$(".sidenav").toggleClass("active");
+		$("header, main").on("click", function () {
+			$(".sidenav").removeClass("active");
+		});
+	});
 });
